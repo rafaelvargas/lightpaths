06965c5ac610a60c88d4c086f1210c59973c3512
Rafael Vargas <rafaelflvargas@gmail.com>
2019-04-26 06:23:49 -0300

    Improve image writing performance

--- src/image.rs
+++ src/image.rs
@@ -1,5 +1,6 @@
 use std::fs::File;
 use std::io::prelude::*;
+use std::io::BufWriter;
 use std::ops::{Index, IndexMut};
 
 pub struct Image {
@@ -20,14 +21,16 @@ impl Image {
     pub fn write(&self, filepath: &str) -> std::io::Result<()> {
         let format = "P3";
         let max_value: u8 = 255;
-        let mut file = File::create(filepath)?;
-        file.write_fmt(format_args!(
+        let file = File::create(filepath)?;
+        let mut buffer = BufWriter::new(file);
+
+        buffer.write_fmt(format_args!(
             "{}\n{} {}\n{}\n",
             format, self.width, self.height, max_value
         ))?;
         for i in 0..self.height as usize {
             for j in 0..self.width as usize {
-                file.write_fmt(format_args!(
+                buffer.write_fmt(format_args!(
                     "{} {} {}\n",
                     self.pixelmap[i][j][0], self.pixelmap[i][j][1], self.pixelmap[i][j][2]
                 ))?;
